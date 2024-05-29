e569062d6980309b154de55ce1bbb2b93336d158
cmliu <admin@cmliu.net>
2024-05-30 04:16:01 +0800

    Site updated: 2024-05-30 04:16:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/28/hello-world/"];function toRandomPost(){
+var posts=["2024/05/30/纯净DNS/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
